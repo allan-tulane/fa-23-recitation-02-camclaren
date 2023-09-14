07bbec413b516cf22d2e54876defc5444c67ad7c
Cameron <cmclaren@tulane.edu>
2023-09-13 22:47:59 -0500

    Updated tests

--- main.py
+++ main.py
@@ -17,8 +17,15 @@ def simple_work_calc(n, a, b):
 
 	Returns: the value of W(n).
 	"""
-	# TODO
-	pass
+# Will (TA) assisted with the simple_work_calc code
+	if n ==1:
+		return a * 1 + n
+	else:
+		new_n = n // b
+		new_w = a * simple_work_calc(new_n, a, b) + n
+		return new_w
+	
+print("result", simple_work_calc(10,2,2))
 
 def work_calc(n, a, b, f):
 	"""Compute the value of the recurrence $W(n) = aW(n/b) + f(n)
@@ -32,8 +39,13 @@ def work_calc(n, a, b, f):
 
 	Returns: the value of W(n).
 	"""
-	# TODO
-	pass
+	
+	if n ==1:
+		return a * 1 + n
+	else:
+		new_n = n // b
+		new_w = a * work_calc(new_n, a, b, f) + f(n)
+		return new_w
 
 def span_calc(n, a, b, f):
 	"""Compute the span associated with the recurrence $W(n) = aW(n/b) + f(n)
@@ -47,8 +59,11 @@ def span_calc(n, a, b, f):
 
 	Returns: the value of W(n).
 	"""
-	# TODO
-	pass
+	
+	if n == 1:
+		return n
+	else:
+		return f(n) + span_calc(n // b, a, b, f)
 
 
 
@@ -63,14 +78,25 @@ def compare_work(work_fn1, work_fn2, sizes=[10, 20, 50, 100, 1000, 5000, 10000])
 	
 	"""
 	result = []
-	for n in input_sizes:
+	for n in sizes:
 		# compute W(n) using current a, b, f
-		result.append((
-			n,
-			work_fn1(n),
-			work_fn2(n)
-			))
+		result.append((n, work_fn1(n), work_fn2(n)))
 	return result
+
+def compare_span(span_fn1, span_fn2, sizes=[10, 20, 50, 100, 1000, 5000, 10000]):
+	"""
+	Compare the values of different recurrences for
+	given input sizes.
+	Returns:
+	A list of tuples of the form
+	(n, work_fn1(n), work_fn2(n), ...)
+	"""
+	result = []
+	for n in sizes:
+	# compute W(n) using current a, b, f
+		result.append((n, span_fn1, span_fn2))
+	return result
+
 
 def print_results(results):
 	""" done """
@@ -79,15 +105,3 @@ def print_results(results):
 							floatfmt=".3f",
 							tablefmt="github"))
 
-def test_compare_work():
-	# curry work_calc to create multiple work
-	# functions taht can be passed to compare_work
-    
-	# create work_fn1
-	# create work_fn2
-
-    res = compare_work(work_fn1, work_fn2)
-	print(res)
-
-def test_compare_span():
-	# TODO
--- test_main.py
+++ test_main.py
@@ -2,11 +2,45 @@ from main import *
 
 def test_simple_work():
 	""" done. """
-	assert work_calc(10, 2, 2) == #TODO
-	assert work_calc(20, 3, 2) == #TODO
-	assert work_calc(30, 4, 2) == #TODO
+	assert work_calc(10, 2, 2) == 52
+	assert work_calc(20, 3, 2) == 473
+	assert work_calc(30, 4, 2) == 1808
+	# new assertions
+	assert work_calc(8, 2, 2) == 48
+	assert work_calc(9, 3, 2) == 147
+	assert work_calc(15, 3, 2) == 171
 
 def test_work():
-	assert work_calc(10, 2, 2,lambda n: 1) == #TODO
-	assert work_calc(20, 1, 2, lambda n: n*n) == #TODO
-	assert work_calc(30, 3, 2, lambda n: n) == #TODO
+	assert work_calc(10, 2, 2, lambda n: 1) == 31
+	assert work_calc(20, 1, 2, lambda n: n*n) == 531
+	assert work_calc(30, 3, 2, lambda n: n) == 573
+
+def curry_work_calc(a, b, f):
+	return lambda n: work_calc(n, a, b, f)
+
+def test_compare_work():
+	# curry work_calc to create multiple work
+	# functions that can be passed to compare_work
+    
+	# create work_fn1
+	work_fn1 = curry_work_calc(3, 2, lambda n: 1)
+	# create work_fn2
+	work_fn2 = curry_work_calc(4, 2, lambda n: n**2)
+
+	result = compare_work(work_fn1, work_fn2)
+	print(result)
+
+def curry_span(a, b, f):
+	return lambda n: span_calc(n, a, b, f)
+
+def test_compare_span():
+	assert span_calc(10, 2, 2, lambda n: 1) == 4
+	assert span_calc(20, 1, 4, lambda n: n * n) == 426
+	assert span_calc(30, 3, 4, lambda n: n) == 38
+
+	span_fn1 = curry_span(2, 2, lambda n: 1)
+	span_fn2 = curry_span(2, 2, lambda n: n ** 2)
+	result = compare_span(span_fn1, span_fn2)
+	print_results(result)
+
+
